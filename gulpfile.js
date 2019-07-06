a660394aef5015cf9f5d7c0afadf98844c879017
Fabio Costa <72fcosta@gmail.com>
2019-07-05 21:50:49 -0300

    ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,75 @@
-var cp = require("child_process")
-var browserSync = require('browser-sync').create('dpn_server')
-var gulp = require('gulp')
-var requireDir = require('require-dir')
-var runSequence = require('run-sequence')
-
+var cp = require("child_process");
+var browserSync = require("browser-sync").create("dpn_server");
+var gulp = require("gulp");
+var requireDir = require("require-dir");
+var runSequence = require("run-sequence");
 
 //--------------------------------------------------------------
 
-requireDir('./tasks')
+requireDir("./tasks");
 
 //--------------------------------------------------------------
 
 gulp.task("jekyll-build", function(done) {
     browserSync.notify("Building Jekyll");
     return cp.spawn("jekyll", ["build"], {stdio: "inherit"})
-    .on("close", done)
-  });
+        .on("close", done);
+});
 
-  gulp.task("jekyll-rebuild", ["jekyll-build"], function() {
-    browserSync.reload()
-  });
+gulp.task("jekyll-rebuild", ["jekyll-build"], function() {
+    browserSync.reload();
+});
 
 //--------------------------------------------------------------
 
-gulp.task('browser-sync', ["jekyll-build"], function () {
+gulp.task("browser-sync", ["jekyll-build"], function () {
     browserSync.init({
-      server: {
-        baseDir: "_site"
-      },
-      open: false,
-      notify: false
-    })
-  })
+        server: {
+            baseDir: "_site"
+        },
+        open: false,
+        notify: false
+    });
+});
 
-  gulp.task('browserSync-reload', function () {
-    browserSync.reload()
-  })
+gulp.task("browserSync-reload", function () {
+    browserSync.reload();
+});
 
 //--------------------------------------------------------------
 
-gulp.task('watch', function () {
-    gulp.watch(["*.html", "_includes/**/*.html", "_layouts/*.html", "_config.yml", "_data/*.yml", "_posts/**/*"], ["jekyll-rebuild"])
-    gulp.watch(['_src/styl/global/*.styl'], ['global-styl'])
-    gulp.watch(['_src/styl/pages/*.styl'], ['pages-styl'])
-    gulp.watch(['_src/styl/includes/*.styl'], ['includes-styl'])
-    gulp.watch(['_src/js/*.js'], ['global-js', 'jekyll-rebuild'])
-    gulp.watch(['_src/img/**/*'], ['sync-img', 'jekyll-rebuild'])
-  })
+gulp.task("watch", function () {
+    gulp.watch(["*.html", "_includes/**/*.html", "_layouts/*.html", "_config.yml", "_data/*.yml", "_posts/**/*"], ["jekyll-rebuild"]);
+    gulp.watch(["_src/styl/global/*.styl"], ["global-styl"]);
+    gulp.watch(["_src/styl/pages/*.styl"], ["pages-styl"]);
+    gulp.watch(["_src/styl/includes/*.styl"], ["includes-styl"]);
+    gulp.watch(["_src/js/*.js"], ["global-js", "jekyll-rebuild"]);
+    gulp.watch(["_src/img/**/*"], ["sync-img", "jekyll-rebuild"]);
+});
 
-  // --------------------------------------------------------------
+// --------------------------------------------------------------
 
-  gulp.task("build", function(callback) {
+gulp.task("build", function(callback) {
     runSequence(
-      'del-css',
-      "sync-favicon",
-      'sync-img',
-      'sync-sprite',
-      "global-styl",
-      "pages-styl",
-      "includes-styl",
-      "third-css",
-      'global-js',
-      "third-js",
-      "jekyll-rebuild",
-      callback
-      );
-    });
-
-    gulp.task('server', function (callback) {
-      runSequence(
-        'browser-sync',
-        'watch',
+        "del-css",
+        "sync-favicon",
+        "sync-img",
+        "sync-sprite",
+        "global-styl",
+        "pages-styl",
+        "includes-styl",
+        "third-css",
+        "global-js",
+        "third-js",
+        "jekyll-rebuild",
         callback
-        )
-      })
+    );
+});
+
+gulp.task("server", function (callback) {
+    runSequence(
+        "browser-sync",
+        "watch",
+        callback
+    );
+});
